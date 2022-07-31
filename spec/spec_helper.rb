77ca89d8f16793eb76ff48fb135816a5b1bc05a3
Lian <biakhong@gmail.com>
2022-07-31 10:56:12 -0400

    Completed Assignment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ RSpec.configure do |config|
   # Use color not only in STDOUT but also in pagers and files
   config.tty = true
 
+  
   # you can do global before/after here like this:
   config.before do
     if Dog.respond_to?(:create_table)
@@ -18,4 +19,5 @@ RSpec.configure do |config|
   config.after do
     DB[:conn].execute("DROP TABLE IF EXISTS dogs")
   end
+
 end
